9ac05ca48f65e90dd0eee696a9c545690a2b1778
Khairul Fahmi <kaifahmi.99@gmail.com>
2023-11-08 10:41:18 +0800

    Latestbranch-K

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_08_014708) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_08_023834) do
   create_table "activities", force: :cascade do |t|
     t.string "activity_title"
     t.text "description"
@@ -97,6 +97,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_08_014708) do
     t.string "last_name"
     t.string "gender"
     t.string "parent_phone"
+    t.string "student_number"
     t.index ["email"], name: "index_students_on_email", unique: true
     t.index ["reset_password_token"], name: "index_students_on_reset_password_token", unique: true
   end
