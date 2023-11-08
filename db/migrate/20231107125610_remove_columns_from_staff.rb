6c47988ce75ceb3bb70182975db61cce3f5aa103
Loai-AL-Sabahi <engineerloai108@gmail.com>
2023-11-08 12:04:53 +0800

    updated branch loai

--- db/migrate/20231107125610_remove_columns_from_staff.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnsFromStaff < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :staffs, :staff_id, :string
-  end
-end
--- db/migrate/20231108013454_drop_students.rb
+++
@@ -1,6 +0,0 @@
-class DropStudents < ActiveRecord::Migration[7.1]
-  def up
-    drop_table :students
-  end
-
-end
