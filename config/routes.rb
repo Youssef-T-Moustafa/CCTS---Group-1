9cb216338ada2570d722db4c748d1765c0579d47
Abdalla Gaafar <abdullahmohmed34@gmail.com>
2023-11-08 10:18:19 +0800

    new graphs

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # config/routes.rb
 Rails.application.routes.draw do
-  resources :posts
   resources :staffs
   get 'home/index'
   root 'home#index'
   get "up" => "rails/health#show", as: :rails_health_check
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,9 @@
 # Activities Activity.destroy(activity_name: "Club E")
 
 
-(1..100).each do |i|
+(1..10).each do |i|
     Post.create(title: "Title #{i}",
         created_at: Time.now - i.days,
         updated_at: Time.now - i.days,
-        views: rand(15..100))
+        views: rand(15..50))
 end
