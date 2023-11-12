e65c092fb8b9265d22bd21d64f1ba417654ce505
Abdalla Gaafar <abdullahmohmed34@gmail.com>
2023-11-12 20:07:07 +0800

    table live, graphs live

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+    before_action :set_staff
+    def set_staff
+        @staffs = Staff.all
+        @staffs_by_gender = Staff.group(:gender).count # this will return a hash like {"male" => 10, "female" => 15}
+    end
+      
+    
 end
