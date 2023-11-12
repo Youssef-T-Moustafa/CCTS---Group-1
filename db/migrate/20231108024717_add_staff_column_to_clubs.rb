f7804b8409281373de512118b4ca3863cfff557b
CV04KaiTo <cv04kaito@gmail.com>
2023-11-12 17:55:56 +0800

    Other commits

--- db/migrate/20231108024717_add_staff_column_to_clubs.rb
+++
@@ -1,5 +0,0 @@
-class AddStaffColumnToClubs < ActiveRecord::Migration[7.1]
-  def change
-    add_reference :clubs, :staff, null: true, foreign_key: true
-  end
-end
--- db/migrate/20231109100736_remove_devise_from_student.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDeviseFromStudent < ActiveRecord::Migration[7.1]
-  def change
-    drop_table :students
-  end
-end
