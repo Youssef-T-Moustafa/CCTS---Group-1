527e11dece9f4c18588dd348ac45d2f388edbc32
exit <exit>
2023-11-26 14:27:45 +0800

    Latest Branch

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,22 @@
-# Create some staff records
-Staff.create!(first_name: "Uknow1", email: "Uknow1@staff.com", password: "12345678", password_confirmation: "12345678")
-Staff.create!(first_name: "Uknow1", email: "Uknow2@staff.com", password: "12345678", password_confirmation: "12345678")
-Staff.create!(first_name: "Uknow1", email: "Uknow2@staff.com", password: "12345678", password_confirmation: "12345678")
+# Assuming this code is in a Rails console or a script
+(1..10).each do |i|
+    Student.create!(
+      first_name: "Student#{i}",
+      last_name: "test",
+      parent_email: "Student#{i}@parent.com",
+      password: "12345678",
+      password_confirmation: "12345678"
+    )
+  end
 
-# Create some student records
-Student.create!(first_name: "Known1", parent_email: "Known1@parent.com", password: "12345678", password_confirmation: "12345678")
-Student.create!(first_name: "Known2", parent_email: "Known2@parent.com", password: "12345678", password_confirmation: "12345678")
-Student.create!(first_name: "Known3", parent_email: "Known3@parent.com", password: "12345678", password_confirmation: "12345678")
+  # Assuming this code is in a Rails console or a script
+(1..3).each do |i|
+    Staff.create!(
+      first_name: "Staff#{i}",
+      last_name: "test",
+      email: "staff#{i}@staff.com",
+      password: "12345678",
+      password_confirmation: "12345678"
+    )
+  end
+  
