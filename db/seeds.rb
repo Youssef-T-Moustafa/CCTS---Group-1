30e337bd106b8f8b82a91d5ca9f0b56577ef1b1e
Abdalla Gaafar <abdullahmohmed34@gmail.com>
2023-12-13 09:54:51 +0800

    Graph3

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,6 +28,31 @@
     password: "12345678",
     password_confirmation: "12345678"
   )
+  end
+
+# db/seeds.rb
+
+# Seed clubs
+clubs = [
+  {name: "Chess Club", description: "A club for chess enthusiasts of all levels"},
+  {name: "Book Club", description: "A club for book lovers who want to share their thoughts and opinions"},
+  {name: "Music Club", description: "A club for music fans who want to learn, play, and listen to different genres and instruments"}
+]
+
+clubs.each do |attributes|
+  Club.find_or_create_by(attributes)
 end
+
+# Seed activities
+activities = [
+  {activity_title: "Chess Tournament", description: "A friendly competition among club members", start_date: "2023-01-15", end_date: "2023-01-16", requested_budget: 500.00, club: Club.find_by(name: "Chess Club"), status: "pending"},
+  {activity_title: "Book Discussion", description: "A discussion on the book 'The Catcher in the Rye' by J.D. Salinger", start_date: "2023-01-20", end_date: "2023-01-20", requested_budget: 100.00, club: Club.find_by(name: "Book Club"), status: "pending"},
+  {activity_title: "Music Workshop", description: "A workshop on how to play the guitar", start_date: "2023-01-25", end_date: "2023-01-25", requested_budget: 300.00, club: Club.find_by(name: "Music Club"), status: "approved"},
+]
+
+activities.each do |attributes|
+  Activity.find_or_create_by(attributes)
+end
+
 
 
