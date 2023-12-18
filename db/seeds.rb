9eb6e1ab3bb3dfb5b71e4aa1939a6dcc15f18c13
Abdalla Gaafar <abdullahmohmed34@gmail.com>
2023-12-18 08:34:50 +0800

    Logo Fixed & Input Validation

--- db/seeds.rb
+++ db/seeds.rb
@@ -81,3 +81,36 @@ Admin.create(
   phone: "0123456789",
   password_digest: BCrypt::Password.create('admin1234')
 )
+
+
+# Assuming this code is in a Rails console or a script
+(1..50).each do |i|
+  Student.create!(
+    first_name: "Student#{i}",
+    last_name: "test",
+    parent_email: "Student#{i}@parent.com",
+    password: "12345678",
+    password_confirmation: "12345678"
+  )
+end
+
+# Assuming this code is in a Rails console or a script
+(1..20).each do |i|
+  Staff.create!(
+    first_name: "Staff#{i}",
+    last_name: "test",
+    email: "staff#{i}@staff.com",
+    password: "12345678",
+    password_confirmation: "12345678"
+  )
+end
+
+(1..3).each do |i|
+Admin.create!(
+  first_name: "Admin#{i}",
+  last_name: "test",
+  email: "Admin#{i}@admin.com",
+  password: "12345678",
+  password_confirmation: "12345678"
+)
+end
