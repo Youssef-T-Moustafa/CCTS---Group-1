dfa0ba4a22913a1084f1f3cc5b3f1af36bd61ccb
exit <exit>
2023-12-24 15:13:01 +0800

    latest working capacity form

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,3 +4,18 @@ import "controllers"
 import "chartkick"
 import "Chart.bundle"
 
+document.addEventListener('DOMContentLoaded', function() {
+    var form = document.getElementById('edit_form_capacity_5'); // Replace with the actual form ID
+    var scrollPosition;
+  
+    if (form) {
+      form.addEventListener('submit', function() {
+        scrollPosition = window.scrollY;
+      });
+    }
+  
+    // After the form is processed and the page is reloaded
+    if (scrollPosition !== undefined) {
+      window.scrollTo(0, scrollPosition);
+    }
+  });
