17dab910f58da455567076fdc921e67d8654ab26
ABDALLA GAAFAR <abdullahmohmed34@gmail.com>
2024-01-03 22:50:31 +0800

    Hi everyone

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 # config/routes.rb
+
 Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy', as: 'logout'
   get 'form_capacities/edit'
@@ -6,7 +7,7 @@ Rails.application.routes.draw do
   resources :inventory_histories
   resources :inventories do
     collection do
-      get :generate_report # Adding generate_report_inventories route
+      get 'generate_report'
     end
   end
   resources :staff_activities
@@ -22,9 +23,9 @@ Rails.application.routes.draw do
       get 'studentsList', to: 'staffs#studentsList'
       get 'staffList', to: 'staffs#staffList' # Adding staffList route
     end
-
+  
     member do
-      get 'stuInfo/:id', to: 'staffs#stuInfo', as: 'stuInfo'
+      get 'stuInfo/:id', to: 'staffs#stuInfo', as: 'stuInfo' 
     end
   end
   resources :club_members
@@ -32,10 +33,12 @@ Rails.application.routes.draw do
   resources :extra_activities
 
   resources :clubs do
+    resource :form_capacity, only: [:edit, :update]
+    get 'edit_form_capacity', to: 'clubs#edit_form_capacity', on: :member
     resources :activities, only: [:new, :create]
     collection do
       get 'displayactivity'
-      get 'mobile_views', to: 'mobile_views', as: 'activities'
+      #This means, if the clubs/xx equals to one of the gets below, lead to the to: page
       get 'finance', to: 'clubs#finance'
       get 'editBudget', to: 'clubs#editBudget'
       get 'generate_report', to: 'clubs#generate_report', format: :csv
@@ -49,6 +52,9 @@ Rails.application.routes.draw do
     end
   end
 
+  
+
+  
   get 'dashboard/generate_report', to: 'dashboard#generate_report', format: :csv, as: 'generate_report_dashboard'
   get '/clubs/show_students/:staff_id/:club_id', to: 'clubs#show_students', as: :show_students
   get 'dashboard/myTable', to: 'dashboards#my_table'
@@ -58,10 +64,14 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy'
-  get 'up' => 'rails/health#show', as: :rails_health_check
+  get "up" => "rails/health#show", as: :rails_health_check
   get 'activities/createActivity', to: 'activities#createActivity', as: 'createActivity'
   post '/upload', to: 'activities#upload'
   get 'activities/:id/media' => 'activities#media', as: :activity_media
   get 'attendances/:id/media' => 'attendances#media', as: :attendance_media
+
   get 'extra_activities/:id/evidence' => 'extra_activities#evidence', as: :extra_activity_evidence
+
+
+
 end
